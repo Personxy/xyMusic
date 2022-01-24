8d40ba4c720e9b51495b426f11b3d634295460ea
xy <1129599386@qq.com>
2022-01-25 00:40:53 +0800

    add:api请求地址变更

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ Vue.use(ElementUI);
 Vue.use(VueAxios, axios);
 Vue.use(animated);
 axios.defaults.baseURL = 'https://netease-cloud-music-api-puce-iota.vercel.app/';
+// axios.defaults.baseURL = 'http://localhost:3000';
+
 new Vue({
   router,
   store,
