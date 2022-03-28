a65e5dab106703d9cace24a94cdfce3157784024
xy <1129599386@qq.com>
2022-03-29 00:07:55 +0800

    fix:修复评论中夹杂英文和中文时不换行的bug

--- src/components/xymusic/musicdetail/comment.vue
+++ src/components/xymusic/musicdetail/comment.vue
@@ -1,43 +1,31 @@
 <template>
-  <div class="comment"
-       v-loading="loading">
+  <div class="comment" v-loading="loading">
     <!-- 评论框 -->
-    <el-dialog title="评论"
-               :visible.sync="centerDialogVisible"
-               width="30%"
-               center
-               v-dialogDrag
-               :modal="false">
-      <textarea maxlength="140"
-                class="inputcomment"
-                :placeholder="placeholderstr"
-                v-model="commentscontent"></textarea>
+    <el-dialog title="评论" :visible.sync="centerDialogVisible" width="30%" center v-dialogDrag :modal="false">
+      <textarea maxlength="140" class="inputcomment" :placeholder="placeholderstr" v-model="commentscontent"></textarea>
       <!-- 剩余字数 -->
       <div class="remainstr">
         <span>{{ 140 - commentscontent.length }}</span>
       </div>
       <!-- 发布评论 -->
-      <div :class="{
+      <div
+        :class="{
           sendcomment: commentscontent.length == 0,
           sendcomment2: commentscontent.length != 0,
           sendcomment3: true,
         }"
-           @click="tocomment(0)">
+        @click="tocomment(0)"
+      >
         评论
       </div>
     </el-dialog>
-    <div class="opencommentdialog"
-         @click="opencommentdialog()">
-      <img src="../../../assets/images/写评论.svg"
-           alt="" />
+    <div class="opencommentdialog" @click="opencommentdialog()">
+      <img src="../../../assets/images/写评论.svg" alt="" />
       <span>写评论</span>
     </div>
-    <div class="allcomments"
-         v-if="hotcomments && hotcomments.length !== 0">
+    <div class="allcomments" v-if="hotcomments && hotcomments.length !== 0">
       <div class="allcommentstitle">全部评论({{ total }})</div>
-      <div class="hotcomment"
-           v-for="item in hotcomments"
-           :key="item.id">
+      <div class="hotcomment" v-for="item in hotcomments" :key="item.id">
         <!-- .用户头像 -->
         <div class="commentuserimg">
           <el-image :src="item.user.avatarUrl"></el-image>
@@ -50,36 +38,23 @@
             <div class="commenttime">{{ item.timeStr }}</div>
             <div class="commentbtn">
               <div class="likebtn">
-                <img src="../../../assets/images/点赞.svg"
-                     alt=""
-                     @click="likecomment(item.commentId, 1)"
-                     v-if="!item.liked" />
-                <img src="../../../assets/images/已点赞.svg"
-                     alt=""
-                     v-else
-                     @click="likecomment(item.commentId, 0)" /><span class="likedCount">{{ item.likedCount }}</span>
+                <img src="../../../assets/images/点赞.svg" alt="" @click="likecomment(item.commentId, 1)" v-if="!item.liked" />
+                <img src="../../../assets/images/已点赞.svg" alt="" v-else @click="likecomment(item.commentId, 0)" /><span class="likedCount">{{ item.likedCount }}</span>
               </div>
-              <img src="../../../assets/images/评论.svg"
-                   alt=""
-                   class="combtn"
-                   @click="opencommentdialog(item.commentId, item.user.nickname)"
-                   style="cursor: pointer" />
+              <img src="../../../assets/images/评论.svg" alt="" class="combtn" @click="opencommentdialog(item.commentId, item.user.nickname)" style="cursor: pointer" />
             </div>
           </div>
         </div>
       </div>
     </div>
     <!-- 更多精彩评论 -->
-    <div class="morecomments"
-         v-if="hotcomments && hotcomments.length !== 0">
+    <div class="morecomments" v-if="hotcomments && hotcomments.length !== 0">
       <span>更多精彩评论&nbsp;></span>
     </div>
     <!-- 最新评论 -->
     <div class="newcommnets">
       <div class="allcommentstitle">最新评论({{ total }})</div>
-      <div class="hotcomment"
-           v-for="item in newcomments"
-           :key="item.id">
+      <div class="hotcomment" v-for="item in newcomments" :key="item.id">
         <!-- .用户头像 -->
         <div class="commentuserimg">
           <el-image :src="item.user.avatarUrl"></el-image>
@@ -88,79 +63,57 @@
         <div class="userandcomment">
           <span class="username">{{ item.user.nickname }}:&nbsp;</span>
           <span class="commencontent">{{ item.content }}</span>
-          <div class="reply"
-               v-if="item.beReplied[0]">
+          <div class="reply" v-if="item.beReplied[0]">
             <span class="replayname">{{ item.beReplied[0].user.nickname }}:&nbsp;</span><span>{{ item.beReplied[0].content }}</span>
           </div>
           <div class="commentfoot">
             <div class="commenttime">{{ item.timeStr }}</div>
             <div class="commentbtn">
               <div class="likebtn">
-                <img src="../../../assets/images/点赞.svg"
-                     alt=""
-                     @click="likecomment(item.commentId, 1)"
-                     v-if="!item.liked" />
-                <img src="../../../assets/images/已点赞.svg"
-                     alt=""
-                     v-else
-                     @click="likecomment(item.commentId, 0)" /><span class="likedCount">{{ item.likedCount }}</span>
+                <img src="../../../assets/images/点赞.svg" alt="" @click="likecomment(item.commentId, 1)" v-if="!item.liked" />
+                <img src="../../../assets/images/已点赞.svg" alt="" v-else @click="likecomment(item.commentId, 0)" /><span class="likedCount">{{ item.likedCount }}</span>
               </div>
-              <img src="../../../assets/images/评论.svg"
-                   alt=""
-                   class="combtn"
-                   @click="opencommentdialog(item.commentId, item.user.nickname)"
-                   style="cursor: pointer" />
-              <img src="../../../assets/images/垃圾箱 (2).svg"
-                   alt=""
-                   class="deletcomment"
-                   v-if="userInfo && item.user.nickname == userInfo.nickname"
-                   @click="deletcomment(item.commentId)" />
+              <img src="../../../assets/images/评论.svg" alt="" class="combtn" @click="opencommentdialog(item.commentId, item.user.nickname)" style="cursor: pointer" />
+              <img src="../../../assets/images/垃圾箱 (2).svg" alt="" class="deletcomment" v-if="userInfo && item.user.nickname == userInfo.nickname" @click="deletcomment(item.commentId)" />
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="nothing"
-         v-if="newcomments.length == 0">没有更多内容了</div>
+    <div class="nothing" v-if="newcomments.length == 0">没有更多内容了</div>
     <!-- 页码 -->
-    <el-pagination :pager-count="9"
-                   :page-size="limit"
-                   layout="prev, pager, next"
-                   :total="total"
-                   background
-                   @current-change="change">
-    </el-pagination>
+    <el-pagination :pager-count="9" :page-size="limit" layout="prev, pager, next" :total="total" background @current-change="change"> </el-pagination>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex';
 // 引入的指令可以是模态框可以拖拽
-import "../../../../src/plugins/directive.js";
+import '../../../../src/plugins/directive.js';
 export default {
-  data () {
+  data() {
     return {
       hotcomments: [],
       newcomments: [],
       total: 0,
       loading: false,
       centerDialogVisible: false,
-      commentscontent: "",
-      placeholderstr: "发表评论",
-      replyid: "",
+      commentscontent: '',
+      placeholderstr: '发表评论',
+      replyid: '',
       t: 1,
       offset: 0,
       limit: 20,
     };
   },
   computed: {
-    ...mapGetters(["songDetails", "cookie", "userInfo", "loginflag"]),
+    ...mapGetters(['songDetails', 'cookie', 'userInfo', 'loginflag']),
   },
   methods: {
     // 获取评论
-    async getmusiccomment () {
+    async getmusiccomment() {
       this.loading = true;
-      const { data } = await this.$http.get("/comment/music", {
+      const { data } = await this.$http.get('/comment/music', {
         params: {
           id: this.songDetails.id,
           limit: this.limit,
@@ -174,36 +127,33 @@ export default {
       this.total = data.total;
       this.loading = false;
     },
-    change (data) {
+    change(data) {
       this.offset = data - 1;
       this.getmusiccomment();
     },
     // 打开评论框
-    opencommentdialog (id, nickname) {
-      this.commentscontent = "";
-      if (!this.userInfo)
-      {
-        this.$store.dispatch("changeloginbar", true);
-        return this.$message.error("请登录后再评论！");
+    opencommentdialog(id, nickname) {
+      this.commentscontent = '';
+      if (!this.userInfo) {
+        this.$store.dispatch('changeloginbar', true);
+        return this.$message.error('请登录后再评论！');
       }
       this.centerDialogVisible = true;
 
-      if (nickname && id)
-      {
-        this.placeholderstr = "回复  " + nickname + ":";
+      if (nickname && id) {
+        this.placeholderstr = '回复  ' + nickname + ':';
         this.replyid = id;
         this.t = 2;
-      } else
-      {
-        this.placeholderstr = "发表评论";
+      } else {
+        this.placeholderstr = '发表评论';
 
         this.t = 1;
-        this.replyid = "";
+        this.replyid = '';
       }
     },
     // 发布评论
-    async tocomment (type) {
-      const { data } = await this.$http.get("/comment", {
+    async tocomment(type) {
+      const { data } = await this.$http.get('/comment', {
         params: {
           t: this.t,
           type: type,
@@ -214,28 +164,26 @@ export default {
           timeStamp: Date.now(),
         },
       });
-      if (data.code == 200)
-      {
-        this.commentscontent = "";
+      if (data.code == 200) {
+        this.commentscontent = '';
         this.centerDialogVisible = false;
         this.$message({
-          message: "评论成功！",
-          type: "success",
+          message: '评论成功！',
+          type: 'success',
         });
-      } else
-      {
+      } else {
         this.centerDialogVisible = false;
-        this.$message.error("评论失败！请稍后再试");
+        this.$message.error('评论失败！请稍后再试');
       }
       setTimeout(() => {
         this.getmusiccomment();
       }, 1000);
     },
     // 删除评论
-    async deletcomment (content) {
-      this.$store.dispatch("changeloginbar", true);
-      if (!this.userInfo) return this.$message.error("请登录后再评论！");
-      const { data } = await this.$http.get("/comment", {
+    async deletcomment(content) {
+      this.$store.dispatch('changeloginbar', true);
+      if (!this.userInfo) return this.$message.error('请登录后再评论！');
+      const { data } = await this.$http.get('/comment', {
         params: {
           t: 0,
           type: 0,
@@ -245,24 +193,22 @@ export default {
           timeStamp: Date.now(),
         },
       });
-      if (data.code == 200)
-      {
+      if (data.code == 200) {
         setTimeout(() => {
           this.getmusiccomment();
         }, 1000);
 
         this.$message({
-          message: "删除成功！",
-          type: "success",
+          message: '删除成功！',
+          type: 'success',
         });
-      } else
-      {
-        this.$message.error("删除失败！请稍后再试");
+      } else {
+        this.$message.error('删除失败！请稍后再试');
       }
     },
     // 点赞评论
-    async likecomment (id, t) {
-      const { data } = await this.$http.get("/comment/like", {
+    async likecomment(id, t) {
+      const { data } = await this.$http.get('/comment/like', {
         params: {
           id: this.songDetails.id,
           cid: id,
@@ -272,30 +218,29 @@ export default {
           timeStamp: Date.now(),
         },
       });
-      if (data.code == 200)
-      {
+      if (data.code == 200) {
         setTimeout(() => {
           this.getmusiccomment();
         }, 1000);
 
         this.$message({
-          message: "操作成功！",
-          type: "success",
+          message: '操作成功！',
+          type: 'success',
         });
       }
     },
   },
-  created () {
+  created() {
     this.getmusiccomment();
   },
   watch: {
-    songDetails () {
+    songDetails() {
       // 歌曲切换时要关闭评论框
       this.centerDialogVisible = false;
       // 页码变为1
       this.offset = 0;
       //默认palceholder
-      this.placeholderstr = "发表评论";
+      this.placeholderstr = '发表评论';
       this.getmusiccomment();
     },
   },
@@ -404,6 +349,8 @@ export default {
         line-height: 150%;
         width: 100%;
 
+        word-break: break-all; //span换行
+
         .username {
           color: #5b85b4;
         }
@@ -476,7 +423,10 @@ export default {
         font-size: 15px;
         line-height: 150%;
         width: 100%;
-
+        word-break: break-all;
+        // .commencontent {
+        //   white-space: pre-wrap;
+        // }
         .username {
           color: #5b85b4;
         }
