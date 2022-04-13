5f93f3abb9136cba29abfaae05653d4b9cb11a91
xy <1129599386@qq.com>
2022-04-13 22:06:04 +0800

    add：cover遮罩

---
+++ src/components/xymusic/playlistpage/cover.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 全屏遮罩 -->
+  <div class="fullscreencover"></div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.fullscreencover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.6);
+  z-index: 1801;
+}
+</style>
